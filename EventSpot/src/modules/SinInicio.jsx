c0f45adbd6496ef1540b2cfa51e522ef7158f92f
Ayalaed <00091722@uca.edu.sv>
2023-12-04 00:04:06 -0600

    :memo:Add:deply de proyecto alalu

--- EventSpot/src/modules/SinInicio.jsx
+++ EventSpot/src/modules/SinInicio.jsx
@@ -5,11 +5,11 @@ import Footer from "./Footer";
 function SinInicio() {
   return (
     <div className="w-full h-screen font-['Epilogue']">
-      <div className="w-full h-100 bg-cover bg-[url('./static/img/background.jpeg')]">
+      <div className="w-full h-100 bg-cover bg-[url('/img/background.jpeg')]">
         <div className="border-b border-shadow w-11/12 m-auto flex flex-row justify-between items-center">
           <Link to="/">
             <img
-              src="/assets/logo-definitivo-2.png"
+              src="/img/logo-definitivo-2.png"
               alt=""
               className="w-30 hover:cursor-pointer"
             />
@@ -26,7 +26,7 @@ function SinInicio() {
       <div className="flex flex-col">
         <div className="flex flex-col md:flex-row w-full py-10 px-6">
           <div className="w-full md:w-1/2 flex align-center justify-center mb-6 md:mb-0">
-            <img src="./src/static/img/bgbit.png" alt="bitmap.png" className="max-w-full h-auto" />
+            <img src="/img/bgbit.png" alt="bitmap.png" className="max-w-full h-auto" />
           </div>
           <div className="w-full md:w-1/2 flex align-center justify-center">
             <div className="flex flex-col gap-10 px-6 md:px-16 text-center">
